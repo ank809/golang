6f0dde892166ff79daf8242ef3fbe58c7d1fd0f0
ank809 <ankita51523@gmail.com>
2024-02-29 23:46:30 +0530

    hello world created

---
+++ Hello_World/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	fmt.Println("Hello World")
+
+
+/*
+Important points:
+
+1. There are 4 ways to run a go program --> go build, go run, go install & go playground.
+
+2. Every go file should start with 'package name' statement.
+
+3. Understanding each line of the code above:x``
+
+ - package main is the package that has been used here. Packages are used to provide code reusability
+ - import statement is used to import a package. Here, fmt package is imported to print the text to standard output.
+ - func main is a special function from where execution of a program starts.
+ - package.Function() is the syntax to call a function in a package. For eg. Println is the function in fmt package.
+*/
+}
